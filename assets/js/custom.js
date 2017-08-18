1ca62ecce5388901dd1a46979be7ea5d8d27fda0
katheer <katheer@eight25media.com>
2017-08-18 17:24:17 +0530

    UI-Katheer

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,14 +9,39 @@ $(document).ready(function() {
 
     var tabactiveTimeline = 1000;
     var tabfadingTimeline = 1200;
+	
+	var mobileClickElement = 'h2';
 
-    $(tabnavWrapper + '>' + tabnavElement).each(function (index) {
-        $(this).attr('tab-trigger', 'tab-item-' + index);
-    });
-
-    $(tabtargetWrapper + '>' + tabtargetElement).each(function (index) {
+	$(tabtargetWrapper + '>' + tabtargetElement).each(function (index) {
         $(this).attr('tab-target', 'tab-item-' + index);
     });
+	
+    $(tabnavWrapper + '>' + tabnavElement).each(function (index) {
+        $(this).attr('tab-trigger', 'tab-item-' + index);
+		var eachlinkAttr = $(this).attr('tab-trigger');
+		var eachlinkName = $(this).text();
+		var eachTabElement =  $(tabtargetWrapper + '>' + tabtargetElement+'[tab-target='+eachlinkAttr+']');	
+		
+		if(mobileClickElement == '') {
+			var mobileAddingLink = '<div mobile-tab-trigger='+eachlinkAttr+'> <h2>'+ eachlinkName +'</h2> </div>';
+		}
+		else {
+			var mobileAddingLink = '<div mobile-tab-trigger='+eachlinkAttr+'> <'+ mobileClickElement +'>'+ eachlinkName +'</'+ mobileClickElement +'> </div>';
+		}
+		
+		eachTabElement.before(mobileAddingLink);
+    });
+
+    /*
+		$(tabtargetWrapper + '>' + tabtargetElement + 'div[mobile-tab-trigger]').each(function (index) {
+			var mobilelinkAttr = $(this).attr('mobile-tab-trigger');	
+			var mobileTabElement = tabtargetWrapper + '>' + tabtargetElement+'[tab-target='+mobilelinkAttr+']';
+			
+			//$(this).insertBefore(mobileTabElement );
+			$(this).addClass('x');
+		}); 
+	*/
+	
 
     $(tabnavWrapper + '>' + tabnavElement+':first-child').addClass(tabnavhighlightClass);
 
@@ -26,6 +51,7 @@ $(document).ready(function() {
 
     setTimeout(activeTabSection, tabactiveTimeline);
 
+    var mobileTrigger = $('*[mobile-tab-trigger]');
 
     $(tabnavWrapper + '>' + tabnavElement).click(function () {
     	var curentTabTrigger= $(this).attr('tab-trigger');
@@ -36,6 +62,18 @@ $(document).ready(function() {
         $(tabnavWrapper + '>' + tabnavElement).removeClass(tabnavhighlightClass);
 
         displayTab.fadeIn(tabfadingTimeline);
+        $(this).addClass(tabnavhighlightClass);
+    });
+
+    $mobileTrigger.click(function () {
+    	var curentMobileTabTrigger= $(this).attr('mobile-tab-trigger');
+        var targetMobileTabs =  $(tabtargetWrapper + '>' + tabtargetElement);
+    	var displayMobileTab =  $(tabtargetWrapper + '>' + tabtargetElement+'[tab-target='+curentMobileTabTrigger+']');
+        
+        targetMobileTabs.slideUp();
+        $mobileTrigger.removeClass(tabnavhighlightClass);
+
+        targetMobileTabs.slideDown();
         $(this).addClass(tabnavhighlightClass);
     });
 });
